24f389f9512b9c9d6fef20bffbf35db71755c3eb
Rasool_hashbari <rasool.hashbari@gmail.com>
2025-08-19 22:20:09 +0330

    add images and create form

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,30 @@
-import Image from "next/image";
-import LoginComponent from "@/componets/login"
-export default function Home() {
-  return (
-    <div className="bg-oceanblue h-screen grid place-items-center">
-      <LoginComponent />
-    </div>
-  );
+"use client";
+import LoginComponent from "@/componets/login";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+import rtlPlugin from "@mui/stylis-plugin-rtl";
+import { CacheProvider } from "@emotion/react";
+import createCache from "@emotion/cache";
 
+export default function Home() {
+  const theme = createTheme({
+    direction: "rtl",
+    palette: {
+      mode: "light",
+    },
+  });
+
+  const cacheRtl = createCache({
+    key: "muirtl",
+    stylisPlugins: [rtlPlugin],
+  });
+
+  return (
+    <CacheProvider value={cacheRtl}>
+      <ThemeProvider theme={theme}>
+        <div className="bg-oceanblue h-screen grid place-items-center">
+          <LoginComponent />
+        </div>
+      </ThemeProvider>
+    </CacheProvider>
+  );
 }
--- src/componets/login.tsx
+++ src/componets/login.tsx
@@ -3,16 +3,29 @@ import React, { useState } from "react";
 import Image from "next/image";
 import logoImage from "@/../public/images/logo.png";
 import bgImage from "@/../public/images/bg.png";
+import TextField from "@mui/material/TextField";
+import Checkbox from "@mui/material/Checkbox";
 
 const LoginComponent = () => {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
+  const [formData, setFormData] = useState({ email: "", password: "" });
+  const [errors, setErrors] = useState<{ email?: string; password?: string }>(
+    {}
+  );
 
-  const handleSubmit = (e: any) => {
-    e.preventDefault();
-    // اینجا می‌تونید لاجیک لاگین رو اضافه کنید
-    console.log("Email:", email, "Password:", password);
+  const label = { inputProps: { "aria-label": "Checkbox demo" } };
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+    setErrors({ ...errors, [e.target.name]: undefined });
   };
+
+  const handleSubmit = (e: React.FormEvent) => {
+    e.preventDefault();
+    const newErrors: { email?: string; password?: string } = {};
+    if (!formData.password) newErrors.password = "این فیلد را پر کنید";
+    setErrors(newErrors);
+  };
+
   return (
     <div className="w-[700px] h-auto bg-white shadow-md rounded-md overflow-hidden">
       <div className="flex">
@@ -20,41 +33,50 @@ const LoginComponent = () => {
           <Image src={bgImage} fill className="object-cover" alt="Background" />
           <div className="absolute inset-0 bg-[#4bc7d2d9]"></div>
         </div>
-        <div className="w-7/12 p-8 flex flex-col items-center justify-start gap-4 ">
-          <div className="relative w-full h-12">
-            <Image src={logoImage} fill alt="Logo" />
+        <div className="w-7/12 p-8 flex flex-col items-center justify-start gap-4">
+          <div className="relative w-full">
+            <Image src={logoImage} width={501} height={99} alt="Logo" />
           </div>
           <div className="bg-[#f0efef] w-full text-center p-2 rounded-4xl">
             <p>سامانه جامع داده پرداز</p>
           </div>
 
           <form
+            dir="rtl"
             onSubmit={handleSubmit}
-            className="bg-white p-6 rounded-xl w-full">
+            className="bg-white rounded-xl w-full">
             <div className="mb-4">
-              <label className="block text-gray-700 mb-2">ایمیل</label>
-              <input
-                type="email"
-                value={email}
-                onChange={(e) => setEmail(e.target.value)}
-                className="w-full px-4 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
-                placeholder="example@mail.com"
-                required
+              <TextField
+                label="پست الکترونیک"
+                size="small"
+                color="info"
+                name="email"
+                value={formData.email}
+                onChange={handleChange}
+                className="w-full text-right"
+                placeholder="info@test.com"
+                error={!!errors.email}
+                helperText={errors.email}
               />
             </div>
-
             <div className="mb-6">
-              <label className="block text-gray-700 mb-2">رمز عبور</label>
-              <input
+              <TextField
+                label="رمز عبور"
                 type="password"
-                value={password}
-                onChange={(e) => setPassword(e.target.value)}
-                className="w-full px-4 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
-                placeholder="رمز عبور خود را وارد کنید"
-                required
+                size="small"
+                color="info"
+                name="password"
+                value={formData.password}
+                onChange={handleChange}
+                className="w-full text-right"
+                error={!!errors.password}
+                helperText={errors.password}
               />
             </div>
-
+            <div className="flex items-center">
+              <Checkbox size="small" {...label} defaultChecked />
+              <p>مرا به خاطر داشته باش</p>
+            </div>
             <button
               type="submit"
               className="w-full bg-[#ff7f26] font-bold text-white py-2 rounded-md cursor-pointer hover:bg-[#f69551]">
