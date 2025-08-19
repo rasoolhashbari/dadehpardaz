75e1a7fbda4ea43e4d440c6f14a3d5661cc40690
Rasool_hashbari <rasool.hashbari@gmail.com>
2025-08-20 02:00:47 +0330

    add text copyright

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,13 @@
 "use client";
+import Image from "next/image";
+import Layer20 from "@/../public/images/Layer20.png";
+// components
 import FormManager from "@/componets/FormManager";
+// mui
 import { createTheme, ThemeProvider } from "@mui/material/styles";
 import rtlPlugin from "@mui/stylis-plugin-rtl";
 import { CacheProvider } from "@emotion/react";
 import createCache from "@emotion/cache";
-
 export default function Home() {
   const theme = createTheme({
     direction: "rtl",
@@ -21,8 +24,32 @@ export default function Home() {
   return (
     <CacheProvider value={cacheRtl}>
       <ThemeProvider theme={theme}>
-        <div className="bg-oceanblue h-screen grid place-items-center">
-          <FormManager />
+        <div className="bg-oceanblue  h-screen grid place-items-center">
+          <div>
+            <FormManager />
+            <div className="flex justify-between pt-5">
+              <p className="text-white text-xs">
+                Copyright c 2019 All Rights Reserved
+              </p>
+              <p className="text-white text-xs">
+                طراحی و پشتیبانی توسط : داده پرداز
+              </p>
+            </div>
+          </div>
+
+          <div className="absolute left-0 flex flex-col self-end ">
+            <Image
+              src={Layer20}
+              width={350}
+              height={500}
+              className="object-cover"
+              alt="Background1"
+            />
+          </div>
+          <div className="absolute bottom-5 w-full flex flex-col gap-5 justify-between items-center ">
+            <div className="h-1 bg-[#71c8d5] w-full"></div>
+            <div className="h-1 bg-[#71c8d5] w-full"></div>
+          </div>
         </div>
       </ThemeProvider>
     </CacheProvider>
--- src/componets/FormManager.tsx
+++ src/componets/FormManager.tsx
@@ -9,11 +9,11 @@ import LoginForm from "@/componets/LoginForm";
 
 const FormManager = () => {
   return (
-    <div className="w-[700px] h-auto bg-white shadow-md rounded-md overflow-hidden">
+    <div className=" relative w-[700px] h-auto bg-white shadow-md rounded-md overflow-hidden">
       <div className="flex">
         <div className="w-5/12 h-auto relative">
           <Image src={bgImage} fill className="object-cover" alt="Background" />
-          <div className="absolute inset-0 bg-[#4bc7d2d9] "></div>
+          <div className="absolute inset-0 z-10 bg-[#4bc7d2d9] "></div>
           <div className="z-10 p-5 relative h-full">
             <NewsletterForm />
           </div>
